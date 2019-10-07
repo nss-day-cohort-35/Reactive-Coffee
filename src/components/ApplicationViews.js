373e89ea541432cfa9c1d9fa4570e5e1bd13db93
Heidi Smith <Heidi@LKs-MacBook-Pro.local>
2019-10-07 17:59:13 -0500

    details and delete button

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Route } from "react-router-dom"
 import CoffeeList from "./coffee/CoffeeList"
+import CoffeeDetails from "./coffee/CoffeeDetails"
+import AddCoffeeForm from "./coffee/AddCoffeeForm"
 
 
 
@@ -10,7 +12,15 @@ class ApplicationViews extends Component {
     return (
       <React.Fragment>
         <Route exact path="/coffee" render={(props) => {
-          return <CoffeeList />
+          return <CoffeeList {...props} />
+        }} />
+
+        <Route exact path="/coffee/:coffeeId(\d+)" render={(props) => {
+          return <CoffeeDetails coffeeId={parseInt(props.match.params.coffeeId)}{...props} />
+        }} />
+
+        <Route exact path="/coffee/new" render={(props) => {
+          return <AddCoffeeForm {...props} />
         }} />
       </React.Fragment>
     )
